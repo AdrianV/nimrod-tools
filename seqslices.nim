ed589444721cd25df4f4671e80d37cfeb113828e
Adrian Veith <adrian@veith-system.de>
2012-06-25 12:31:48 +0200

    complete change of code. Looks much simpler now. Also changed from ref semantic to value semantic

--- seqslices.nim
+++ seqslices.nim
@@ -18,211 +18,139 @@
 ## are unlinked from its parent
 ##
 ## the current implementation is merly a proof of concept and uncomplete. Bugs expected !
-
+## thanks to the tipps of Araq - code completly changed, using more nimrod features 
 
 type
-  PtrSeqSlice [T] = ptr TSeqSlice[T]
-  PSeqSlice* [T] = ref TSeqSlice[T]
-  TSliceableSeq {.final, pure.}[T] = object
-    arr: seq[T]
-    head, tail: PtrSeqSlice[T]
+  TSeqData [T] = array[0 .. 1_000_000, T]
+  TGenSeqInfo {.pure.} [T] = object
+    len, space: int
+    data: TSeqData [T]
+  PGenSeq[T] = ref TGenSeqInfo[T]
+  PSeqData [T] = ptr TSeqData [T]
   TSeqSlice* {.final, pure.}[T] = object
-    base: ptr T
-    next, prev: PtrSeqSlice[T]
-    arr: ptr TSliceableSeq[T]
-    ofs: int32
-    len: int32
-    own: ref TSliceableSeq[T]
-  
+    base: PSeqData [T]
+    lenX: int
+    data: seq[T]
+  PSeqSlice* [T] = ref TSeqSlice[T]
 
 
-proc finalize[T] (me: ref TSliceableSeq[T]) 
-proc remove[T] (me: PSeqSlice[T]) 
+when sizeof(int) == 4:
+  const seqUniqueFlag = 1 shl 31
 
-proc initSliceableSeq[T](me: ref TSliceableSeq[T], len: int) {.inline.} =
-  newSeq(me.arr, len)
+when sizeof(int) == 8:
+  const  seqUniqueFlag = 1 shl 63
     
-proc newSliceableSeq [T] (me: var ref TSliceableSeq[T], len: int) =
-  new(me, finalize[T])
-  initSliceableSeq(me, len)  
+proc len* [T] (me: TSeqSlice[T]): int {.inline.} = 
+  # return then len of a TSeqSlice
+  return me.lenX and not seqUniqueFlag
 
-proc setBounds[T](me: PSeqSlice[T], arr: var seq[T], a, b: int) {.inline.} = 
-  let l = arr.len
-  if a < l :
-    me.ofs = a
-    me.len = min(l - a, b - a + 1)
-    me.base = addr arr[a]
+proc rebase[T] (me: var TSeqSlice[T], x: int) {.inline.} =
+  me.base = cast[PSeqData [T]](addr cast[PGenSeq[T]](me.data).data[x])
+
+proc all* [T] (fseq: seq[T]): TSeqSlice[T] =
+  # copy a seq[T] to a TSeqSlice[T]
+  result.data= fseq
+  result.lenX = result.data.len or seqUniqueFlag
+  rebase(result, 0)
+  # result.base = cast[PSeqData [T]](addr cast[PGenSeq[T]](result.data).data)
+
+proc all* [T] (fseq: TSeqSlice[T]): TSeqSlice[T] =
+  # TSeqSlices are shallow copied
+  shallowcopy(result.data, fseq.data)
+  result.lenX = result.data.len
+  result.base = fseq.base
+
+proc slice* [T] (fseq: seq[T], x: TSlice[int]): TSeqSlice[T] =
+  # create a slice from a seq[T]
+  let l = fseq.len
+  if x.a < l :
+    let l2 = min(l - x.a, x.b - x.a + 1)
+    newSeq(result.data, l2)
+    for i in 0.. <l2:
+      result.data[i] = fseq[i + x.a]
+    result.lenX = l2 or seqUniqueFlag
+  else : # empty
+    result.data= @[]
+    result.lenX = seqUniqueFlag
+  rebase(result, 0)
+
+proc ofs* [T] (me: TSeqSlice[T]): int {.inline.} =
+  return (cast[int](me.base) - cast[int](addr cast[PGenSeq[T]](me.data).data)) div sizeof(T)
   
-proc all* [T] (fseq: seq[T]): PSeqSlice[T] =
-  # make a slice from an existing seq. The seq is copied
-  new(result, remove[T])
-  new(result.own, finalize[T])
-  result.own.arr = fseq
-  setBounds(result, result.own.arr, 0, fseq.len)
-
-proc slice* [T] (fseq: seq[T], x: TSlice[int]): PSeqSlice[T] =
-  # make a slice from an existing seq with bounds. The seq is copied
-  new(result, remove[T])
-  new(result.own, finalize[T])
-  result.own.arr = fseq
-  setBounds(result, x.a, x.b)
-
-proc newSeqSlice* [T](me: var PSeqSlice[T], newLen: int) =
-  # create a new slicable slice
-  new(result, remove[T])
-  new(result.own, finalize[T])
-  newSeq(result.own.arr, newLen)
-  setBounds(result, 0, newLen)
-
-
-proc append[T] (me: var TSliceableSeq[T], arr: ptr TSeqSlice[T]) {.inline.} =
-  arr.arr = addr me
-  arr.next = me.head
-  me.head = arr
-  if me.tail == nil :
-    me.tail = me.head
-  else:
-    arr.next.prev = arr
-
-proc initSeqSlice[T] (me: var TSeqSlice[T], parent: var TSliceableSeq[T]) {.inline.} =
-  me.arr = addr parent
-  append(parent, addr me)
-
-proc newFrom [T](me: var PSeqSlice[T], parent: var TSliceableSeq[T]) {.inline.} =
-  new(me, remove[T]) 
-  initSeqSlice(me[], parent)
+proc slice* [T] (fseq: TSeqSlice[T], x: TSlice[int]): TSeqSlice[T] =
+  # create a shallow slice from a slice
+  shallowcopy(result.data, fseq.data)
+  let l = fseq.len
+  if x.a < l :
+    result.lenX = min(l - x.a, x.b - x.a + 1)
+    rebase(result, x.a + fseq.ofs)
+    # result.base = cast[PSeqData [T]](addr cast[PGenSeq[T]](result.data).data[x.a + fseq.ofs])
   
-proc slice* [T] (me: PSeqSlice[T], x:TSlice[int]): PSeqSlice[T] =
-  # slice a slicable slice. no data is copied here
-  if me.arr != nil :
-    # inherit parent slice
-    newFrom(result, me.arr[])
-    setBounds(result, me.arr.arr, x.a + me.ofs, x.b + me.ofs)
-  else :
-    # me is parent 
-    newFrom(result, me.own[])
-    setBounds(result, me.own.arr, x.a, x.b)
-
-proc `[]`* [T] (me: PSeqSlice[T], x:TSlice[int]): PSeqSlice[T] {.inline.} =
-  # same as slice for conviniance
-  return me.slice(x)
-
+proc `[]`* [T] (fseq: TSeqSlice[T], x: TSlice[int]): TSeqSlice[T] {.inline.} =
+  # same as slice
+  return fseq.slice(x)
+  
 template checkbounds() = 
   when not defined(release) :
     if x < 0 or x >= me.len : raise newException(EInvalidIndex, "index out of bounds")
   nil
 
-proc getX [T] (me: var TSeqSlice[T], x: int) : T {.inline.} =
-  checkbounds()
-  return cast[ptr T](cast[int](me.base) + x * sizeof(T)) []
-
-proc `[]`* [T] (me: PSeqSlice[T], x: int) : T {.inline.} =
+proc `[]`* [T] (me: TSeqSlice[T], x: int) : T {.inline.} =
   # array like access of element x
   checkbounds()
-  return cast[ptr T](cast[int](me[].base) + x * sizeof(T)) []
+  return me.base[x]
   
-iterator items*[T](me: PSeqSlice[T]): T {.inline.} =
+iterator items*[T](me: TSeqSlice[T]): T {.inline.} =
   # iterate over all items of a slice
-  var i = 0
-  while i < me.len:
-    yield me[i]
-    inc(i)
+  var base = me.base
+  let last = addr base[me.len]
+  while base < last:
+    yield base[0]
+    base = cast[PSeqData [T]](addr base[1])
 
-iterator pairs*[T](me: PSeqSlice[T]): tuple[key: int, val: T] {.inline.} =
+iterator pairs*[T](me: TSeqSlice[T]): tuple[key: int, val: T] {.inline.} =
   # iterate over all (index, item) pairs of a slice
   var i = 0
-  while i < me.len:
+  let len = me.len
+  while i < len:
     yield (i, me[i])
     inc(i)
 
-
-template unbindImpl =    
-  var me = arr.arr
-  if arr.prev == nil : # me.head == arr 
-    me.head = arr.next
-    if me.head == nil: me.tail = nil
-    else: arr.next.prev = nil
-  else: 
-    arr.prev.next = arr.next # arr.prev != nil
-    if arr.next != nil:
-      arr.next.prev = arr.prev
-    else: me.tail = arr.prev
-  arr.arr = nil
-  # echo cast[int](me.head), " ", cast[int](me.tail)
-  
-proc unbind[T] (arr: var TSeqSlice[T]) {.inline.} =
-  unbindImpl
-
-proc unbind[T] (arr: ref TSeqSlice[T]) {.inline.} =
-  unbindImpl
-  
-proc remove[T] (me: PSeqSlice[T]) =
-  # echo "remove"
-  if me.arr != nil:
-    unbind(me)
-    
 proc makeUnique[T] (me: var TSeqSlice[T]) = 
-  # echo "makeUnique"
-  if me.arr != nil :
-    newSliceableSeq(me.own, me.len)
-    # newSeq(me.own, me.len)
-    for i in 0.. < me.len :
-      me.own.arr[i] = getX(me, i)
-    me.base = addr me.own.arr[0]
-    unbind(me)
+  let l = me.len
+  var temp: seq[T]
+  newSeq(temp, l)
+  for i in 0.. < l :
+    temp[i] = me[i]
+  shallowCopy(me.data, temp)
+  rebase(me, 0)
+  me.lenX = l or seqUniqueFlag
 
-proc `[]=`* [T] (me: PSeqSlice[T], x: int, val : T) {.inline.} =
+proc `[]=`* [T] (me: var TSeqSlice[T], x: int, val : T) {.inline.} =
   # change element x of the slice. changes are not passed to the parent
   # the slice is unlinked from the parent
   checkbounds()
-  if me.arr != nil : makeUnique(me[])
-  cast[ptr T](cast[int](me[].base) + x * sizeof(T)) [] = val
-    
-proc rebase[T] (me: var TSliceableSeq[T], x: PSeqSlice[T]) {.inline.} = 
-  x.base = cast[ptr T](cast[int](addr me.arr[0]) + x.ofs * sizeof(T))
+  if (me.lenX and seqUniqueFlag) == 0: makeUnique(me)
+  me.base[x] = val
 
-proc beforeSetLen[T] (me: var TSliceableSeq[T], newLen: int) = 
-  # echo "before SetLen"
-  var x = me.head
-  while x != nil :
-    if x.ofs + x.len > newLen:
-      makeUnique(x[])
-    x = x.next
-  
-proc rebase[T] (me: var TSliceableSeq[T]) = 
-  # echo "rebase"
-  var x = me.head
-  let l = me.arr.len
-  let base = if l > 0 : cast[int](addr me.arr[0]) else : 0
-  while x != nil :
-    if x.ofs + x.len <= l:
-      x.base = cast[ptr T](base + x.ofs * sizeof(T))
-    else:
-      makeUnique(x[])
-    x = x.next
-    
-proc setLen[T](me: var TSliceableSeq[T], newlen: int) =
-  # change the len of a parent. if a child is longer than the
-  # newLen, it is unlinked
-  beforeSetLen(me, newLen)
-  let old = (if me.arr.len != 0 : addr (me.arr[0]) else : nil)
-  setLen(me.arr, newLen)
-  if newLen != 0 or old != addr me.arr[0] : rebase(me)
+proc setLen* [T](me: var TSeqSlice[T], newLen: Int) =
+  # change the len of a slice
+  var l = me.data.len
+  if newLen > l:
+    var temp: seq[T]
+    l = l - me.ofs
+    newSeq(temp, newLen)
+    for i in 0.. < l :
+      temp[i] = me.base[i]
+    shallowCopy(me.data, temp)
+    rebase(me, 0)
+    me.lenX = newLen or seqUniqueFlag
+  else :
+    me.lenX = newLen or (me.lenX and seqUniqueFlag)
 
-proc setLen* [T] (me : PSeqSlice[T], newLen: Int) {.inline.} =
-  if me.arr != nil : setLen(me.arr[], newLen)
-  else : 
-    setLen(me.own[], newLen)
-    me.len = newLen
+proc `len=` * [T](me: var TSeqSlice[T], newLen: Int) {.inline.} =
+  setLen(me, newLen)
 
-proc finalize[T] (me: ref TSliceableSeq[T]) =
-  # echo "finalize"
-  when true:
-    var x = me.head
-    while x != nil :
-      makeUnique(x[])
-      x = x.next
-    
 when isMainModule:
 
   proc show[T] (arr: T, info: string = "") =
@@ -232,30 +160,78 @@ when isMainModule:
       stdout.write(delim)
       stdout.write($el)
       delim = ','
+    if delim == '[' : stdout.write(delim)
     echo ']'
 
-  var test = @[1,2,3,4,5,6,7,8,9].all()
+  var test = all(@[1,2,3,4,5,6,7,8,9])
 
-  test.show("initial: ")
+  test.show("test: ")
 
-  var stest = test.slice(2..4)
-  stest.show("[2..4]: ")
-  var t2 = test[1..6]
-  t2.show("[1..6]: ")
-  var t3 = t2[0..1000]
-  t3.show("[0..1000] cut of:")
-  test[3] = 12
-  test.show("test[3] == 12: ")
-  stest.show("stest[1] == 12 now: ")
-  stest[1] = 5
-  stest.show("stest[1] changed: ")
-  t2.show("change is not seen by others: ")
-  test.show("dito: ")
-  test.setLen(7)
-  test.show("len changed: ")
-  t3.show("untouched: ")
-  test = nil
-  t2 = nil
-  GC_fullCollect()
-  stest.show("still alive: ")
-  t3.show("me too: ")
+  var t2= test.all()
+  
+  t2.show("t2: ")
+
+  test.len = 0
+  t2.show("t2: ")
+  test.show("test: ")
+  test.len = 7
+  test.show("test: ") # test and t2 are still using the same array
+
+  test[2] = 314
+  t2.show("t2: ")
+
+  t2.len = 19 # now they are unlinked
+  t2.show("t2: ")
+  test.show("test: ")
+  test[2] = 66
+  test.show("test: ")
+  t2.show("t2: ")
+  
+  var t3 = @[1,2,3,4,5,6,7,8,9].slice(1..100)
+
+  t3.show("t3: ")
+
+  var t4 = t3.slice(2..6)
+
+  t4.show("t4: ")
+
+  t3[2] = 99
+
+  t3.show("t3: ")
+  t4.show("t4: ")
+  
+  proc getTest(): TSeqSlice[int] =
+    return @[1,2,3,4,5,6,7,8,9].slice(1..5)
+
+  t4 = getTest()
+  t4.show("t4: ")
+  t3.show("t3: ")    
+
+  proc oldTest() =
+    var test = @[1,2,3,4,5,6,7,8,9].all()
+
+    test.show("initial: ")
+
+    var stest = test.slice(2..4)
+    stest.show("[2..4]: ")
+    var t2 = test[1..6]
+    t2.show("[1..6]: ")
+    var t3 = t2[0..1000]
+    t3.show("[0..1000] cut of:")
+    test[3] = 12
+    test.show("test[3] == 12: ")
+    stest.show("stest[1] == 12 now: ")
+    stest[1] = 5
+    stest.show("stest[1] changed: ")
+    t2.show("change is not seen by others: ")
+    test.show("dito: ")
+    test.setLen(7)
+    test.show("len changed: ")
+    t3.show("untouched: ")
+    # test = nil
+    # t2 = nil
+    GC_fullCollect()
+    stest.show("still alive: ")
+    t3.show("me too: ")
+
+oldTest()
